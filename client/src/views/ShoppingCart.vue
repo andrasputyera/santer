9c753a1cd8c69340bc23647c75cd37e420d0f02a
andrasputyera <andras.putyera@gmail.com>
2021-05-06 15:34:15 +0200

    modified shopping cart page by creating list + item components

--- client/src/views/ShoppingCart.vue
+++ client/src/views/ShoppingCart.vue
@@ -1,28 +1,21 @@
 <template>
     <div id="page-wrap">
       <h1>Shopping Cart</h1>
-      <div
-       v-for="product in cartItems"
-       :key="product.id"
-       class="product-container"
-      >
-        <img :src="product.imageUrl" class="product-image">
-        <div class="details-wrap">
-          <h3>{{ product.name }}</h3>
-          <p>{{ product.price }}</p>
-        </div>
-        <button class="remove-button">Remove from Cart</button>
-      </div>
+      <ProductsList :products="cartItems" />
       <h3 id="total-price">Total: ${{ totalPrice }}</h3>
       <button id="checkout-button">Proceed to Checkout</button>
     </div>
 </template>
 
 <script>
-import { cartItems } from '../fake-data'
+import { cartItems } from '../fake-data';
+import ProductsList from '../components/ProductsList.vue';
 
 export default {
     name: 'ShoppingCart',
+    components: {
+      ProductsList
+    },
     data() {
       return {
         cartItems
@@ -54,29 +47,5 @@ export default {
 
   #checkout-button {
     width: 100%;
-  }
-
-  .product-container {
-    align-content: 'center';
-    border-bottom: 1px solid #ddd;
-    display: flex;
-    padding: 16px;
-    width: 100%;
-  }
-
-  .product-image {
-    flex: 1;
-    height: 100px;
-    max-width: 100px;
-  }
-
-  .details-wrap {
-    padding: 0 16px;
-    flex: 3;
-  }
-
-  .remove-button {
-    flex: 1;
-    margin: auto;
   }
 </style>
