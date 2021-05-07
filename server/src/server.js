a5a5dccd4eea25c1d018b87604225795c6605ad5
andrasputyera <andras.putyera@gmail.com>
2021-05-07 12:05:24 +0200

    set up route parameters & getting data from the server

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,90 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
+const products = [{
+    id: '123',
+    name: 'Whoosh Cleaner',
+    price: '9.99',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+    imageUrl: '/images/whoosh.jpeg',
+    averageRating: '9.8',
+  }, {
+    id: '234',
+    name: 'Eveo Brush',
+    price: '8.97',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+    imageUrl: '/images/eveo.jpg',
+    averageRating: '9.3',
+  }, {
+    id: '345',
+    name: 'Innovera Cloth',
+    price: '5.49',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+    imageUrl: '/images/innovera.jpeg',
+    averageRating: '8.5',
+  }, {
+    id: '456',
+    name: 'Screen Mom Cleaner',
+    price: '14.99',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+    imageUrl: '/images/screen-mom.jpeg',
+    averageRating: '9.2',
+  }, {
+    id: '567',
+    name: 'Tech Armor Kit',
+    price: '13.99',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+    imageUrl: '/images/tech-armor.jpeg',
+    averageRating: '8.7',
+  }, {
+    id: '678',
+    name: 'Endust Wipe + Spray',
+    price: '21.49',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.',
+    imageUrl: '/images/endust.jpg',
+    averageRating: '8.9',
+  }];
+  
+  export const cartItems = [
+    products[0],
+    products[2],
+    products[3],
+  ];
+
+
 
 const app = express();
+app.use(express.json());
 
-app.get('/hello', (req, res) => {
-    res.send('Hello!');
+app.get('/api/products', (req, res) => {
+    res.status(200).json(products);
 });
+
+app.get('/api/users/:userId/cart', (req, res) => {
+    res.status(200).json(cartItems);
+});
+
+app.get('/api/products/:productId', (req, res) => {
+    const { productId } = req.params;
+    const product = products.find((product) => product.id === productId);
+    if (product) {
+        res.status(200).json(product);
+    } else {
+        res.status(404).json('Could not find the product!')
+    }
+});
+
+// app.get('/hello', (req, res) => {
+//     res.send('Hello!');
+// });
+
+// app.post('/hello', (req, res) => {
+//     res.send(`Hello ${req.body.name}`)
+// });
+
+// app.get('/hello/:name', (req, res) => {
+//     res.send(`Hello ${req.params.name}!`);
+// });
 
 app.listen(8000, () => {
     console.log('Server is listening on port 8000');
